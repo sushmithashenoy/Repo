aa382cfb4b16fb3289bd0b682b461071fd6eabfe
shenoy <s.shenoy@interfacema.de>
2019-02-23 20:34:23 +0100

    attributes for booking confirmation set

--- BookingApplication/src/com/booking/service/BookingServlet.java
+++ BookingApplication/src/com/booking/service/BookingServlet.java
@@ -1,10 +1,10 @@
 package com.booking.service;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.booking.dao.UserDAO;
 import com.booking.model.Event;
@@ -27,9 +27,15 @@ public class BookingServlet extends HttpServlet {
 			guest.setContact(request.getParameter("contact"));
 			System.out.println("in booking servlet");
 			Event event = UserDAO.makeBooking(guest, request.getParameter("from"), request.getParameter("to"));
-			response.sendRedirect("BookingConfirmation.jsp"); // logged-in page
+			request.setAttribute("name", guest.getName());
+			request.setAttribute("room", event.getRoom().getRoomNo());
+			request.setAttribute("booking", event.getReservationID());
+			request.setAttribute("from", event.getCheckIn());
+			request.setAttribute("to", event.getCheckOut());
+			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("BookingConfirmation.jsp");
+			dispatcher.forward(request, response);
 		} catch (Exception e) {
-			response.sendRedirect("Error.jsp"); // error page
+			response.sendRedirect("BookingError.jsp"); // error page
 		}
 
 		catch (Throwable theException) {
